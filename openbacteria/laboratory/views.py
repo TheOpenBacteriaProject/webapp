c4b2871cf4bfa96078a34f3292831c811d56e80d
Enrique Casale Linde <kicalin@gmail.com>
2018-04-17 14:46:13 +0200

    Pequeños arreglos. Primeras vistas patatas.

--- openbacteria/laboratory/views.py
+++ openbacteria/laboratory/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from .models import Bacterium
 
 # Create your views here.
+
+
+def mostrar_bacterias(request):
+    lista_bacterias = Bacterium.objects.all()
+    output = ''
+    output += ', '.join([bacteria.name for bacteria in lista_bacterias])
+    output += '\n'
+    output += ', '.join([bacteria.description for bacteria in lista_bacterias])
+    return HttpResponse(output)
--- openbacteria/openbacteria/urls.py
+++ openbacteria/openbacteria/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+#App imports
+from laboratory.views import mostrar_bacterias
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bacterias/', mostrar_bacterias),
 ]
